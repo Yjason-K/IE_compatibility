0f54c60d6b1e8b6435e837d2147e33459456f98d
JasonKim <gomgom0324@gmail.com>
2024-03-12 21:30:21 +0900

    Asnync 컴포넌트 추가

---
+++ src/AsyncComponent.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+interface posts{
+  "userId": number,
+  "id": number,
+  "title": string,
+  "body": string
+}
+
+const AsyncComponent = () => {
+  const [data, setData] = useState<posts[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Async Component</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AsyncComponent;
