0dd79dcd660c77e80fe4059595db28ae3d571042
김영재 <gomgom0324@gmail.com>
2024-03-13 21:56:00 +0900

    vite legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    legacy({
-      targets: ['IE >= 11'],
-      additionalLegacyPolyfills: [
-        'regenerator-runtime/runtime',
-        'react-app-polyfill/ie11',
-        'react-app-polyfill/stable',
-        'core-js/stable',
-      ]
-    }),
     react()
   ],
 })
